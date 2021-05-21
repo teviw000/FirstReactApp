edb98300627218d68d862a0faba09deb751d8a3b
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-05-21 12:18:57 -0500

    Added new component for Expenses where ExpenseItem.js will reside.

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,9 @@
 import './ExpenseDate.css';
 
 function ExpenseDate(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const month = props.DATE.toLocaleString("en-US", { month: "long" });
+  const day = props.DATE.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.DATE.getFullYear();
 
   return (
     <div className='expense-date'>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -11,7 +11,7 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       {/* Self closing element vs <ExpenseDate></ExpenseDate> if no content between open/close text*/}
-      <ExpenseDate date = {props.date}/>
+      <ExpenseDate DATE = {props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
