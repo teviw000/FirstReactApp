03bf637d893a01538249b0311212cf4dfac78ad4
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-05-21 10:36:30 -0500

    Added normal JavaScript Logic to Component ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,23 @@ import "./ExpenseItem.css";
 // props is the object call of ExpenseItem in App.js
 // We call the Object and their attibutes as one parameter
 function ExpenseItem(props) {
+  // better to instantiate it it vs <div>{props.date.toLocaleString("en-US", {month: "long"})}</div>
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      {/*
+            - output value dynamically called by props again.
+            - build in method of date: toLocaleString
+            <div>{props.date.toLocaleString("en-US", { month: "long" })}</div>
+      */}
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
